2de30dfd59b44dd7737e77f8cdbb566e23e6c03d
Shivani <shbisht1@gmail.com>
2022-04-10 09:27:36 +0530

    added files

---
+++ frontend/src/screens/common/AboutUsScreen.js
@@ -0,0 +1,26 @@
+import Navigation from "../../components/Navigation";
+import Footer from "../../components/Footer"; 
+
+const AboutUsScreen = () => {
+    return (
+        <div>
+            <Navigation/>
+            <div className="main1">
+            <h3>About Us : </h3>
+            <p>Launched in 2022, Our technology platform connects customers, 
+                restaurant partners and delivery partners, serving their multiple needs. 
+                Customers use our platform to search and discover restaurants, 
+                order food delivery and make payments to restaurants. 
+                On the other hand, we provide restaurant partners with 
+                industry-specific marketing tools which enable them to engage and acquire 
+                customers to grow their business while also providing a reliable and 
+                efficient last mile delivery service. We also operate a one-stop procurement 
+                solution, Hyperpure, which supplies high quality ingredients and kitchen 
+                products to restaurant partners. We also provide our delivery partners with 
+                transparent and flexible earning opportunities.
+                </p>
+        </div>
+        </div>
+    );
+}
+export default AboutUsScreen
