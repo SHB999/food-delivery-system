c4e86826900bf3f2845c1d6f36fc03833d21b4ee
Shivani <shbisht1@gmail.com>
2022-04-10 08:28:28 +0530

    Removed Folder

--- backend/src/main/java/com/app/dao/OrderAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.OrderAddress;
-
-public interface OrderAddressRepository extends JpaRepository<OrderAddress, Integer> {
-	OrderAddress findByOrderId(int oId);
-}
